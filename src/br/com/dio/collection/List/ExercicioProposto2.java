3e154bbb94790a2e9867aa49b187b77cb5a0abdf
RayneMarquez <rmpiedade@hotmail.com>
2022-04-03 16:09:52 -0300

    Commit inicial - Exercicios propostos

---
+++ src/br/com/dio/collection/List/ExercicioProposto2.java
@@ -0,0 +1,50 @@
+package br.com.dio.collection.List;
+
+import java.util.*;
+
+public class ExercicioProposto2 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        List<String> perguntas = new ArrayList<>();
+        List<String> respostas = new ArrayList<>();
+
+        perguntas.add("Telefonou para vítima?");
+        perguntas.add("Esteve no local do crime?");
+        perguntas.add("Mora perto da vítima?");
+        perguntas.add("Devia para vítima?");
+        perguntas.add("Já trabalhou com a vítima?");
+
+        System.out.println("Perguntas: ");
+        for (String perg: perguntas) {
+            System.out.println(perg);
+            respostas.add(leitor.next().toLowerCase(Locale.ROOT));
+        }
+
+        System.out.println(respostas.toString());
+
+        int count = 0;
+        Iterator<String> contador = respostas.iterator();
+        while(contador.hasNext()){
+            String resp = contador.next();
+            if(resp.contains("s")) {
+                count ++;
+            }
+        }
+
+        switch(count) {
+            case 2:
+                System.out.println(">> SUSPEITA <<");
+                break;
+            case 3:
+            case 4:
+                System.out.println(">> CÚMPLICE <<");
+                break;
+            case 5:
+                System.out.println(">> ASSASSINO <<");
+                break;
+            default:
+                System.out.println(">> INOCENTE <<");
+                break;
+        }
+    }
+}
---
+++ src/br/com/dio/collection/List/ExerciocioProposto1.java
@@ -0,0 +1,63 @@
+package br.com.dio.collection.List;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class ExerciocioProposto1 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        List<Double> listaTemperaturaMedia = new ArrayList<Double>();
+
+        for(int i=0;i<6;i++){
+            System.out.println("Digite a temperatura do mês " + (i+1) + " : ");
+            listaTemperaturaMedia.add(leitor.nextDouble());
+        }
+
+        System.out.println(listaTemperaturaMedia.toString());
+
+        double somaTemperaturas = 0.0;
+        Iterator<Double> iterator = listaTemperaturaMedia.iterator();
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            somaTemperaturas += next;
+        }
+
+        double temperaturaMediaSemestral = somaTemperaturas/listaTemperaturaMedia.size();
+        System.out.printf("Temperatura Media Semestral: %.2f", temperaturaMediaSemestral);
+        System.out.println("\n----------");
+
+        for (Double temp: listaTemperaturaMedia) {
+            if(temp > temperaturaMediaSemestral){
+                int index = listaTemperaturaMedia.indexOf(temp);
+                switch (index){
+                    case 1:
+                        System.out.printf("Janeiro %.2f \n", temp);
+                        break;
+                    case 2:
+                        System.out.printf("Fevereiro %.2f \n", temp);
+                        break;
+                    case 3:
+                        System.out.printf("Março %.2f \n", temp);
+                        break;
+                    case 4:
+                        System.out.printf("Abril %.2f \n", temp);
+                        break;
+                    case 5:
+                        System.out.printf("Maio %.2f \n", temp);
+                        break;
+                    case 6:
+                        System.out.printf("Junho %.2f \n",temp);
+                        break;
+                    default:
+                        System.out.println("Não houve temperatura acima da média.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
+
+
